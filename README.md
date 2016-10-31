50ad673a4c3d6cba70f12b5938c17259c79168f0
Gerald Hurdle <hurdleg@algonquincollege.com>
2016-10-31 09:52:36 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+## Async Task ##
+  How to define background tasks with class AsyncTask.
+
+## Source Code ##
+
+  Available from GitHub:
+
+  https://github.com/hurdleg/AsyncTask.git
+
+## Reference ##
+
+  Based on AsyncTask in _Connecting Android Apps to RESTful Web Services_ with David Gassner
---
+++ app/src/main/java/com/algonquincollege/hurdleg/doorsopenottawa/MainActivity.java
@@ -0,0 +1,68 @@
+package com.algonquincollege.hurdleg.doorsopenottawa;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * How to define background tasks with class AsyncTask.
+ *
+ * @author Gerald.Hurdle@AlgonquinCollege.com
+ *
+ * Reference: based on AsyncTask in "Connecting Android Apps to RESTful Web Services" with David Gassner
+ */
+public class MainActivity extends Activity {
+
+    private TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//		Initialize the TextView for vertical scrolling
+        output = (TextView) findViewById(R.id.textView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_do_task) {
+            MyTask task = new MyTask();
+            task.execute("Param 1", "Param 2", "Param 3");
+        }
+        return false;
+    }
+
+    protected void updateDisplay(String message) {
+        output.append(message + "\n");
+    }
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            updateDisplay("Starting task");
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            return "Task complete";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            updateDisplay(result);
+        }
+    }
+}
